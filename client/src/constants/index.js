3f7d6b146490a7e9bb73eb196d9b2d26e57d4b32
golibdev <gnarzullayev2000@gmail.com>
2022-12-20 11:36:31 +0500

    updated

--- client/src/constants/index.js
+++ client/src/constants/index.js
@@ -1,4 +1,4 @@
-export const baseUrl = "http://localhost:4000/api/v1/"
+export const baseUrl = "https://mvp-api.narzullayev.uz/api/v1/"
 export const token = localStorage.getItem('token');
 export const headers = { headers: { Authorization: `Bearer ${token}` }};
 export const serverUrl = "http://localhost:5000";
